cf54c7552f2689bcffd22f590acc2dbc5f9eb271
joesunesis <57965428+joesunesis@users.noreply.github.com>
2024-12-05 12:55:48 +0000

    Updated category file
    Category pages can filter out its items

--- pages/category.tsx
+++ pages/category.tsx
@@ -1,39 +1,11 @@
-import { MaxWidthWrapper } from '@/app/components';
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import Categories from '@/app/components/Categories';
 
-interface Category {
-  id: number;
-  name: string;
-}
-
-export default function Categories() {
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  useEffect(() => {
-    setCategories([
-      { id: 1, name: 'Electronics' },
-      { id: 2, name: 'Clothing' },
-      { id: 3, name: 'Home Appliances' },
-    ]);
-  }, []);
+export default function Category() {
 
   return (
-    <MaxWidthWrapper className='p-4'>
-      <h2 className="text-lg font-bold mb-4">Categories</h2>
-      <ul>
-        {categories.map((category) => (
-          <li key={category.id} className="mb-4">
-            <div className="p-4 bg-white shadow rounded-lg">
-              <h3 className="text-lg">
-                <Link href={`/category/${category.name.toLowerCase()}`}>
-                  {category.name}
-                </Link>
-              </h3>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </MaxWidthWrapper>
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Categories</h2>
+      <Categories />
+    </div>
   );
 };
