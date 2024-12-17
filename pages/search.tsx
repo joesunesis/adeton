3cc058cd0f79c81b2927f9e8a83f820d2fa2371d
joesunesis <57965428+joesunesis@users.noreply.github.com>
2024-12-17 08:07:13 +0000

    Prompt search (#10)
    * Update search.tsx
    
    Pruning dead code.

--- pages/search.tsx
+++ pages/search.tsx
@@ -11,7 +11,6 @@ export default function SearchPage() {
   const [searchValue, setSearchValue] = useState('');
   const router = useRouter();
 
-  // Fetch data once on component mount
   useEffect(() => {
     const fetchData = async () => {
       const data = await getData("items");
@@ -22,13 +21,12 @@ export default function SearchPage() {
     fetchData();
   }, [getData]);
 
-  // Handle search logic
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setSearchValue(value);
 
     if (!value) {
-      setSearchResults(null); // Reset results if input is empty
+      setSearchResults(null);
       return;
     }
 
@@ -46,7 +44,7 @@ export default function SearchPage() {
   return (
     <div>
       {/* Header */}
-      <div className="flex items-center justify-around mb-4">
+      <div className="flex items-center space-x-4 mb-4">
         <button onClick={() => router.back()} className="text-blue-500 bg-[#2c2674] rounded-2xl p-4">
           <span className="hidden md:block">←</span> Back
         </button>
@@ -55,20 +53,16 @@ export default function SearchPage() {
           type="text"
           placeholder="Teams/Players, Leagues"
           value={searchValue}
-          onChange={handleSearch} // Call the handler
+          onChange={handleSearch}
           className="w-9/12 md:w-full bg-[#2c2674] p-4 rounded-lg outline-none text-white placeholder-gray-500"
         />
       </div>
 
       {/* Search Results */}
-      <div className="mt-4 grid grid-cols-2 gap-4">
-        {searchResults && searchResults.length > 0 ? (
-          searchResults.map((item) => (
-            <ItemCard key={item.itemId} item={item} />
-          ))
-        ) : (
-          <h1 className="text-white text-center col-span-2">No items found</h1>
-        )}
+      <div className="mt-4 grid grid-cols-2 md:grid-cols-4 gap-4">
+        {searchResults?.map((item) => (
+          <ItemCard key={item.itemId} item={item} />
+        ))}
       </div>
     </div>
   );
